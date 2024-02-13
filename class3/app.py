eacc58f5e485d01b72cf0479ddaa0a0811d973be
Valdo Neves <valdo@alfasense.com.br>
2024-02-13 20:28:30 -0300

    [MAIN] New class 3

---
+++ class3/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__, static_folder='../static') # static_folder indica o local onde o diretorio static esta
+@app.route('/')
+def index():
+    fruits = ["apple", "mango", "orange"]
+    return render_template('index.html', fruits=fruits)
+
+@app.route("/home")
+def home():
+    return redirect(url_for("index"))
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+#debug = True refresh automaticaly
+#port define port number
